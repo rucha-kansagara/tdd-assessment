4c29ee75a2b0e907a2beafeb8679280f4bfc2604
rucha kansagara <ruchachikani@gmail.com>
2024-10-24 14:20:53 +0530

    modified logic as per requierment

--- src/main/java/com/tss/wicp/StringCalculator.java
+++ src/main/java/com/tss/wicp/StringCalculator.java
@@ -9,18 +9,21 @@ public class StringCalculator {
             return 0;
         }
         StringBuilder negativeNumbers = new StringBuilder();
-        int sum = Arrays.stream(DelimiterStrategyFactory.getDelimiterStrategy(n).split(n))
-                .mapToInt(Integer::parseInt)
-                .peek(num -> {
-                    if (num < 0) {
-                        negativeNumbers.append(num).append(",");
+
+
+       String[] sum = DelimiterStrategyFactory.getDelimiterStrategy(n).split(n);
+       Arrays.stream(sum).mapToInt(Integer::parseInt)
+               .forEach(i ->{
+                    if(i<0){
+                        negativeNumbers.append(i).append(",");
                     }
-                })
-                .sum();
+               });
         if (negativeNumbers.length() > 0) {
             negativeNumbers.setLength(negativeNumbers.length() - 1);
             throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers);
         }
-        return sum;
+       int ans = Arrays.stream(sum).mapToInt(Integer::parseInt).sum();
+
+        return ans;
     }
 }
